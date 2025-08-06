1f844d9dda61030f63ff1870bab9df222eb918cb
Danilo Galisteu <danilogalisteu@gmail.com>
2025-08-06 15:32:20 -0300

    ruff format

--- app/main.py
+++ app/main.py
@@ -62,15 +62,15 @@ def match_deep(input_line, pattern):
             raise ValueError("invalid pattern")
         pattern_set = pattern[2:pattern_end]
         this_match = input_line[0] not in pattern_set
-        if len(pattern) > pattern_end+1:
-            next_match = match_deep(input_line[1:], pattern[pattern_end+2:])
-            if pattern[pattern_end+1] == "+":
+        if len(pattern) > pattern_end + 1:
+            next_match = match_deep(input_line[1:], pattern[pattern_end + 2 :])
+            if pattern[pattern_end + 1] == "+":
                 more_match = match_deep(input_line[1:], pattern)
                 return (next_match or more_match) and this_match
-            if pattern[pattern_end+1] == "?":
-                zero_match = match_deep(input_line, pattern[pattern_end+2:])
+            if pattern[pattern_end + 1] == "?":
+                zero_match = match_deep(input_line, pattern[pattern_end + 2 :])
                 return zero_match or (next_match and this_match)
-        next_match = match_deep(input_line[1:], pattern[pattern_end+1:])
+        next_match = match_deep(input_line[1:], pattern[pattern_end + 1 :])
         return next_match and this_match
 
     if pattern.startswith("["):
@@ -79,15 +79,15 @@ def match_deep(input_line, pattern):
             raise ValueError("invalid pattern")
         pattern_set = pattern[2:pattern_end]
         this_match = input_line[0] in pattern_set
-        if len(pattern) > pattern_end+1:
-            next_match = match_deep(input_line[1:], pattern[pattern_end+2:])
-            if pattern[pattern_end+1] == "+":
+        if len(pattern) > pattern_end + 1:
+            next_match = match_deep(input_line[1:], pattern[pattern_end + 2 :])
+            if pattern[pattern_end + 1] == "+":
                 more_match = match_deep(input_line[1:], pattern)
                 return (next_match or more_match) and this_match
-            if pattern[pattern_end+1] == "?":
-                zero_match = match_deep(input_line, pattern[pattern_end+2:])
+            if pattern[pattern_end + 1] == "?":
+                zero_match = match_deep(input_line, pattern[pattern_end + 2 :])
                 return zero_match or (next_match and this_match)
-        next_match = match_deep(input_line[1:], pattern[pattern_end+1:])
+        next_match = match_deep(input_line[1:], pattern[pattern_end + 1 :])
         return next_match and this_match
 
     this_match = pattern[0] == input_line[0]
